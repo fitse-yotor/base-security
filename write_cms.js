0c0af331caeef0dd031b556ba0232b70dc969a32
fitse-yotor <fitseyotor@gmail.com>
2026-04-21 16:28:40 +0300

    final

---
+++ write_cms.js
@@ -0,0 +1,22 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const content = [
+  "import { create } from 'zustand';",
+  "",
+  "// CMS Types",
+  "",
+  "export interface HeroContent {",
+  "  badge: string;",
+  "  title: string;",
+  "  highlightWord: string;",
+  "  subtitle: string;",
+  "  primaryButtonText: string;",
+  "  secondaryButtonText: string;",
+  "  backgroundImage: string;",
+  "}",
+  ""
+].join('\n');
+
+fs.writeFileSync('src/app/lib/cms-store.ts', content, 'utf8');
+console.log('done');
